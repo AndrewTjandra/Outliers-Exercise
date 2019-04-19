796375c8c32e340fea216e320e9df6f1d622b7df
AndrewTjandra <49597796+AndrewTjandra@users.noreply.github.com>
2019-04-19 10:56:26 -0700

    Update outliers.js

--- outliers.js
+++ outliers.js
@@ -1,17 +1,3 @@
-/*
-Given a string of even and odd numbers, find which is the sole even number or the sole odd number.
-
-The return value should be 1-indexed, not 0-indexed.
-
-Examples :
-detectOutlierValue("2 4 7 8 10"); // => 3 - Third number is odd, while the rest of the numbers are even
-detectOutlierValue("1 10 1 1");  //=> 2 - Second number is even, while the rest of the numbers are odd
-*/
-//create function detectOutlierValue
-//Go through array to cound the number of even and odd values
-//check which number (even or odd) is equal to 1
-//return the number
-
 function detectOutlierValue(numbers){
   var numOfEven = 0;
   var numOfOdd = 0;
